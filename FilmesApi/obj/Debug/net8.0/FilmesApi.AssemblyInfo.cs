bbc98c9a3171d830cf14640451f0aa8a6575ac68
ryanbrito <ryanbragabrito@gmail.com>
2024-09-25 16:29:52 -0300

    Source folder renaming

--- FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3631fc11ee591d4c5ec6e110f50e6707cfe460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbfeb98d92986ab748857021a019ba9b3219212")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
