95102254764284d1c4f74209ccb634c76fd7c61d
ryanbrito <ryanbragabrito@gmail.com>
2024-09-25 14:10:03 -0300

    First Commit

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O titulo do filme é obrigatório!")]
+        public String Titulo { get; set; }
+        [Required(ErrorMessage = "O Genero do filme é obrigatório!")]
+        [StringLength(50, ErrorMessage = "O tamanho de genero não pode exceder 50 caracteres")]
+        public String Genero { get; set; }
+        [Required(ErrorMessage = "A duração do filme é obrigatória.")]
+        [Range(70, 600, ErrorMessage = "A Duração deve ser entre 70 e 600 minutos")]
+    }
+}
---
+++ FilmesApi/Program.cs
@@ -0,0 +1,45 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine("Args:");
+Console.WriteLine(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
+
+builder.Services.AddDbContext<FilmeContext>(
+    opts => 
+    opts.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    )
+);
+
+builder.Services.AddAutoMapper(
+    AppDomain.CurrentDomain.GetAssemblies()
+);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
